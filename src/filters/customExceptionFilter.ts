e4976f332edd86cc39d848447a406a4f769819eb
Grigory Perevalov <perevalov@omnicomm.ru>
2021-12-30 02:22:46 +0500

    Add exceptions logger

--- src/filters/customExceptionFilter.ts
+++ src/filters/customExceptionFilter.ts
@@ -5,10 +5,12 @@ export default class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: ForbiddenException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-
-    return response.status(502).send({
+    const info = {
       status: 502,
       message: exception.message,
-    });
+    };
+
+    console.warn(info);
+    return response.status(502).json(info);
   }
 }
